32ed4a80d12e3b7453f24ecf0eb19d95840b3a33
PaoloNavarro <navarropaolo2020@gmail.com>
2023-05-17 07:28:34 -0600

    Fucionamineto login y registro

--- app/src/main/java/sv/edu/catolica/lv/mipisto/Login.java
+++ app/src/main/java/sv/edu/catolica/lv/mipisto/Login.java
@@ -1,14 +1,91 @@
 package sv.edu.catolica.lv.mipisto;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
 
 public class Login extends AppCompatActivity {
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private Button buttonLogin;
+    private Button btnRegistro;
+
+
+    private DatabaseHelper databaseHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // Inicializar instancias de vistas
+        editTextEmail = findViewById(R.id.correo_login);
+        editTextPassword = findViewById(R.id.contra_login);
+        buttonLogin = findViewById(R.id.btnInicioSesion);
+        btnRegistro = findViewById(R.id.btnRegistro);
+
+
+        // Inicializar instancia de DatabaseHelper
+        databaseHelper = new DatabaseHelper(this);
+
+        // Configurar el click listener del botón de inicio de sesión
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Obtener el correo electrónico y la contraseña ingresados
+                String email = editTextEmail.getText().toString().trim();
+                String password = editTextPassword.getText().toString().trim();
+
+                // Validar si los campos están vacíos
+                if (email.isEmpty() || password.isEmpty()) {
+                    Toast.makeText(Login.this, "Por favor, ingresa todos los campos", Toast.LENGTH_SHORT).show();
+                } else {
+                    // Verificar las credenciales en la base de datos
+                    if (checkCredentials(email, password)) {
+                        // Iniciar sesión exitosa, redirigir a la siguiente actividad
+                        Intent intent = new Intent(Login.this, MainActivity.class);
+                        startActivity(intent);
+                        finish(); // Cerrar la actividad de inicio de sesión
+                    } else {
+                        // Credenciales incorrectas, mostrar mensaje de error
+                        Toast.makeText(Login.this, "Credenciales incorrectas", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+
+        });
+
+        //ir a registro
+        btnRegistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this, Registro.class);
+                startActivity(intent);
+                finishAffinity(); // Finaliza todas las actividades en la pila, incluyendo la de inicio de sesión
+            }
+        });
+    }
+
+    private boolean checkCredentials(String email, String password) {
+        SQLiteDatabase db = databaseHelper.getReadableDatabase();
+
+        // Realizar una consulta a la base de datos para verificar las credenciales
+        Cursor cursor = db.rawQuery("SELECT * FROM User WHERE email = ? AND password = ?",
+                new String[]{email, password});
+
+        boolean result = cursor.getCount() > 0;
+
+        cursor.close();
+        db.close();
+
+        return result;
     }
 }
--- app/src/main/java/sv/edu/catolica/lv/mipisto/Registro.java
+++ app/src/main/java/sv/edu/catolica/lv/mipisto/Registro.java
@@ -1,14 +1,79 @@
 package sv.edu.catolica.lv.mipisto;
-
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import sv.edu.catolica.lv.mipisto.R;
 
 public class Registro extends AppCompatActivity {
+
+    private EditText nombreEditText, apellidoEditText, telefonoEditText, correoEditText, contraEditText;
+    private Button registroButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        nombreEditText = findViewById(R.id.nombre);
+        apellidoEditText = findViewById(R.id.apellido);
+        telefonoEditText = findViewById(R.id.telefono);
+        correoEditText = findViewById(R.id.correo);
+        contraEditText = findViewById(R.id.contra);
+        registroButton = findViewById(R.id.btnRegistro);
+
+        registroButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Obtener los valores ingresados por el usuario
+                String nombre = nombreEditText.getText().toString();
+                String apellido = apellidoEditText.getText().toString();
+                String telefono = telefonoEditText.getText().toString();
+                String correo = correoEditText.getText().toString();
+                String contra = contraEditText.getText().toString();
+
+                // Validar los campos (por ejemplo, verificar que no estén vacíos)
+                if (nombre.isEmpty() || apellido.isEmpty() || telefono.isEmpty() || correo.isEmpty() || contra.isEmpty()) {
+                    Toast.makeText(Registro.this, "Por favor, complete todos los campos", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                // Realizar el registro en la base de datos (aquí debes implementar tu lógica de base de datos)
+                boolean exitoRegistro = registrarUsuario(nombre, apellido, telefono, correo, contra);
+
+                if (exitoRegistro) {
+                    Toast.makeText(Registro.this, "Registro exitoso", Toast.LENGTH_SHORT).show();
+                    // Aquí puedes redirigir al usuario a otra actividad o realizar alguna acción adicional
+                    Intent intent = new Intent(Registro.this, Login.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    Toast.makeText(Registro.this, "Error en el registro. Inténtalo de nuevo", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
+    private boolean registrarUsuario(String nombre, String apellido, String telefono, String correo, String contra) {
+        DatabaseHelper databaseHelper = new DatabaseHelper(this);
+        SQLiteDatabase db = databaseHelper.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put("user_name", nombre);
+        values.put("password", contra);
+        values.put("email", correo);
+
+        long resultado = db.insert("User", null, values);
+
+        db.close();
+
+        return resultado != -1;
+    }
+
 }
